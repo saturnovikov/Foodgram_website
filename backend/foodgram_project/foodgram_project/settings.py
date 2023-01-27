5c8a63ab8299b685fd444611d9c18ec23db0100c
Антон Новиков <avnovikov.project@ya.ru>
2023-01-27 17:28:27 +0300

    правки после первого ревью. вариант 1

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -12,7 +12,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '(dx$+u_tei%9*e5+g9z*y#14v0ro(-5m9qbhu()_95hm%1w53!'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
