38355f9e581c2557163e2eb3cf57766a6dc6b599
Антон Новиков <avnovikov.project@ya.ru>
2023-03-15 17:35:34 +0300

    update settings.py allowed_host v2

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -8,11 +8,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 AUTH_USER_MODEL = 'users.User'
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '84.201.164.171']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
